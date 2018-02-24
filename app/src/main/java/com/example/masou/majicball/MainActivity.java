17144b499be0092008be6e01d4e369a7e7825e0f
Masoud <Masoud.Homayouni@gmail.com>
2018-02-24 09:51:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/masou/majicball/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.masou.majicball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView image_eightBall= findViewById(R.id.image_eightBall);
+        final int [] ballArray= {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        Button ask=findViewById(R.id.luck_Button);
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumber=new Random();
+                int number=randomNumber.nextInt(5);
+                image_eightBall.setImageResource(ballArray[number]);
+            }
+        });
+    }
+}
